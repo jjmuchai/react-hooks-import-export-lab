2ef2fa4be62fc1389b04241a9dcd6886ebae04f5
jjmuchai <jjmuchai151@gmail.com>
2025-04-16 12:57:00 +0300

    My updates to the project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { username, city } from "../data/user.js";
+
+
 function Home() {
   return (
     <div id="home">
@@ -9,3 +12,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
